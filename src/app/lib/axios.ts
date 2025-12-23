c86556b2d5427ee951a40ded38889f6c9f30a8bf
Kaique Prado <kiqprado@outlook.com>
2025-12-23 11:25:31 -0300

    updating server details.

--- src/app/lib/axios.ts
+++ src/app/lib/axios.ts
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333'
+  baseURL: 'https://plannerapi-r2pn.onrender.com/'
 })
 
-/* https://plannerapi-p95p.onrender.com */
